11a675203a7ed403faa06a2d89da0ac2608b3f6f
Vestertech <vesteerwood@gmail.com>
2022-09-12 17:20:41 +0100

    Added Middlewares to my Express server

--- myApp.js
+++ myApp.js
@@ -5,10 +5,17 @@ let app = express();
 
 console.log("Hello World")
 
+
+// Starting Express Server
 // app.get('/', (req, res) => {
 //     res.send('Hello Express');
 // })
 
+
+// Serving Middleware to our Express
+app.use(express.static(__dirname + '/public'))
+
+// serving HTML files
 app.get('/', (req,res) => {
     res.sendFile(__dirname + '/views/index.html');
 })
